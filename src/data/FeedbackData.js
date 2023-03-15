84c5502253bd7c1585b1ff61940d51842685a2bb
miketeow <27071053+miketeow@users.noreply.github.com>
2023-03-15 19:37:21 +0800

    initial

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: "1",
+    rating: 7,
+    text: "lorem ipsum dolor sit amet",
+  },
+  {
+    id: "2",
+    rating: 8,
+    text: "lorem ipsum dolor sit amet",
+  },
+  {
+    id: "3",
+    rating: 9,
+    text: "lorem ipsum dolor sit amet",
+  },
+];
+
+export default FeedbackData;
